85eaa60d61d9c693237b48a401d9fa173780162b
Mr Silué <siluessirabana@gmail.com>
2021-05-20 13:15:50 +0000

    dernier1

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,4 +1,5 @@
 import scala.collection.immutable.Map
+import scala.collection.immutable.ListMap
 
 object ExamenBigData {
 
@@ -9,20 +10,20 @@ object ExamenBigData {
   }
 
   def liste_double():Unit = {
-    val liste : Map[String,String] = Map(
-      "ecommercemag.fr"->" ",
-      "https://www.journalducm.com/contact/"->"Payant",
-      "https://www.zatsaz.com/"->" ",
-      "https://www.lerevenu.com/"->"",
-      "https://www.cadre-dirigfdeant-magazine.com/"->"Payant",
-      "https://www.silcon.fr/services/contact#annoncer"->"Payant",
-      "https://www.channelbiz.fr/nous-contact"->" ",
-      "https://www.itespresso.fr"->" ",
-      "https://www.industrie-mag.com/article4.html"->"Invité",
-      "https://www.jesuisundev.com/article-invite/"->"Invité",
-      "https://www.numerama.com"->" "
+    val Maliste = List(("ecommercemag.fr"," "),
+      ("https://www.journalducm.com/contact/","Payant"),
+      ("https://www.zatsaz.com/"," "),
+      ("https://www.lerevenu.com/"," "),
+      ("https://www.cadre-dirigfdeant-magazine.com/","Payant"),
+      ("https://www.silicon.fr/services/contact#annoner","Payant"),
+      ("https://www.channelbiz.fr/nous-contacter/"," "),
+      ("https://www.itespresso.fr/"," "),
+      ("https://www.industrie-mag.com/article4.html","invite"),
+      ("https://www.jesuisundev.com/article-invite/","invite"),
+      ("https://www.numerama.com/"," ")
     )
-  val tab =liste.toList
+
+    val liste2  = (ListMap(Maliste:_*).keys.toList.map(l => l.replace("https://www.","").split("/")(0) ) zip ListMap(Maliste:_*).values).toList // funsionner liste de tuple
 
   }
   def main(args: Array[String]): Unit = {
