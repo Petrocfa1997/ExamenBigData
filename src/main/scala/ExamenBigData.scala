3eeb225b8e110a53ca168c651f3b50b58de5afc3
Mr Silué <siluessirabana@gmail.com>
2021-05-20 12:49:07 +0000

    dernier

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,3 +1,5 @@
+import scala.collection.immutable.Map
+
 object ExamenBigData {
 
   def retournerListe(a : List[String]) : List[String]= {
@@ -6,7 +8,24 @@ object ExamenBigData {
     return lis
   }
 
-  def main(args: Array[String]): Unit = {
+  def liste_double():Unit = {
+    val liste : Map[String,String] = Map(
+      "ecommercemag.fr"->" ",
+      "https://www.journalducm.com/contact/"->"Payant",
+      "https://www.zatsaz.com/"->" ",
+      "https://www.lerevenu.com/"->"",
+      "https://www.cadre-dirigfdeant-magazine.com/"->"Payant",
+      "https://www.silcon.fr/services/contact#annoncer"->"Payant",
+      "https://www.channelbiz.fr/nous-contact"->" ",
+      "https://www.itespresso.fr"->" ",
+      "https://www.industrie-mag.com/article4.html"->"Invité",
+      "https://www.jesuisundev.com/article-invite/"->"Invité",
+      "https://www.numerama.com"->" "
+    )
+  val tab =liste.toList
 
+  }
+  def main(args: Array[String]): Unit = {
+    retournerListe(List("julien","Paul","jean","rac","trec","joel","ed","chris","maurice"))
   }
 }
