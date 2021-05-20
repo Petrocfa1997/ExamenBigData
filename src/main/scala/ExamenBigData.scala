9e7c335e889d00a73327e5d460c2a54bede734ba
Mr Silué <siluessirabana@gmail.com>
2021-05-20 11:59:08 +0000

    commitretournerListe

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,8 +1,9 @@
 object ExamenBigData {
 
   def retournerListe(a : List[String]) : List[String]= {
-    val lis: List[String] = lis.filter(l=>l.endsWith("n"))
-  return lis
+    val lis: List[String] = a.filter(l=>l.endsWith("n"))
+
+    return lis
   }
 
   def main(args: Array[String]): Unit = {
